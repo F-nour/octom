169c73fc2e4c3db31fe6a1d790f7223b140745c7
Nour-eddine <92918272+F-nour@users.noreply.github.com>
2022-11-23 18:44:36 +0100

    first correction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './_pages/dashboard/dashboard.component';
+import { ActivityComponent } from './_pages/activity/activity.component';
+import { FilesComponent } from './_pages/files/files.component';
+import { MessengerComponent } from './_pages/messenger/messenger.component';
+import { EbooksComponent } from './_pages/ebooks/ebooks.component';
+import { SettingsComponent } from './_pages/settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    DashboardComponent,
+    ActivityComponent,
+    FilesComponent,
+    MessengerComponent,
+    EbooksComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
