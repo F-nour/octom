733ade17681bb0e5324f0fa65e5e8487aeab019d
Nour-eddine <fattoumne@protonmail.com>
2022-12-07 10:15:27 +0100

    octom - update project

--- octom/src/app/app-routing.module.ts
+++ octom/src/app/app-routing.module.ts
@@ -1,7 +1,54 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './_pages/dashboard/dashboard.component';
+import { ActivityComponent } from './_pages/activity/activity.component';
+import { EbooksComponent } from './_pages/ebooks/ebooks.component';
+import { SettingsComponent } from './_pages/settings/settings.component';
+import { MessengerComponent } from './_pages/messenger/messenger.component';
+import { FilesComponent } from './_pages/files/files.component';
+
+const parentTitle = 'Octom - '
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    title: 'dashboard'
+
+  },
+  {
+    path: 'activity',
+    component: ActivityComponent,
+    title: parentTitle + 'activity'
+  },
+  {
+    path: 'ebooks',
+    component: EbooksComponent
+  },
+  {
+    path:'settings',
+    component: SettingsComponent,
+    title: parentTitle + 'settings'
+  },
+  {
+    path:'messenger',
+    component: MessengerComponent,
+    title: parentTitle + 'messenger'
+  },
+  {
+    path:'files',
+    component: FilesComponent,
+    title: parentTitle + 'files'
+  },
+  {
+    path: '',
+    redirectTo: '/messenger',
+    pathMatch: 'full'
+  }
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
