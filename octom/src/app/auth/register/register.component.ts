5680c55880451186ad6fb86f41868609a2b094f6
Nour-eddine <fattoumne@protonmail.com>
2022-12-15 11:50:09 +0100

    initialise reactive form

--- octom/src/app/auth/register/register.component.ts
+++ octom/src/app/auth/register/register.component.ts
@@ -1,24 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
   register!: string;
 
   nameForm: string;
 
-  constructor() {
+  registerForm!: FormGroup
+
+  constructor(private formBuilder: FormBuilder) {
     this.nameForm = "register"
   }
 
   ngOnInit(): void {
+
+    this.registerForm = this.formBuilder.group ({
+      name: [null],
+      firstname: [null],
+      username: [null],
+      password: [null],
+      confirmPassword: [null]
+    })
   }
 
-  registerListener(register: string) {
-    this.register = register;
+  get name() { return this.registerForm.get('name'); }
+  get firstname() { return this.registerForm.get('firstname'); }
+  get username() { return this.registerForm.get('username'); }
+  get password() { return this.registerForm.get('password'); }
+  get confirmPassword() { return this.registerForm.get('confirmPassword'); }
+
+
+  submit() {
+    alert("bravo!");
+    return;
   }
 }
