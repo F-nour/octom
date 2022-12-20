87cbdbf5f569449a5206f9b8c4f6ef2641eecf33
Nour-eddine <fattoumne@protonmail.com>
2022-12-20 16:49:41 +0100

    update fontAwesome module

--- octom/src/app/core/core.module.ts
+++ octom/src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
