ecdbbf7a2c6663d71492032f1079a08931dcb923
Nour-eddine <fattoumne@protonmail.com>
2023-01-05 13:31:04 +0100

    set default language to french

--- octom/src/app/app.module.ts
+++ octom/src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { AuthModule } from './auth/auth.module';
-import { CoreModule } from './core/core.module';
-import { ActivityModule } from './activity/activity.module';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { EbooksModule } from './ebooks/ebooks.module';
-import { MessengerModule } from './messenger/messenger.module';
-import { SettingsModule } from './settings/settings.module';
-
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AuthModule} from './auth/auth.module';
+import {CoreModule} from './core/core.module';
+import {ActivityModule} from './activity/activity.module';
+import {DashboardModule} from './dashboard/dashboard.module';
+import {EbooksModule} from './ebooks/ebooks.module';
+import {MessengerModule} from './messenger/messenger.module';
+import {SettingsModule} from './settings/settings.module';
 
 
 @NgModule({
@@ -29,8 +27,13 @@ import { SettingsModule } from './settings/settings.module';
     MessengerModule,
     SettingsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'fr-FR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 
-export class AppModule { }
+export class AppModule {
+}
